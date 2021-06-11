92b77aea1a06ebae975ca0a39b0fae2648230893
rohit <rohit.chandekar786@gmail.com>
2021-06-11 10:27:26 +0530

    managing states

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,8 +9,42 @@ class CartItem extends React.Component{
             qty: 1,
             img: ''
         }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
     }
+    increaseQuantity = ()=>{
+        //Naive approach
+        // this.state.qty = this.state.qty+1;
 
+        //setState form 1
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // });
+
+        //setState form 2
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+        })
+    }
+    decreaseQuantity = ()=>{
+        // this.setState({
+        //     qty: this.state.qty - 1
+        // });
+        this.setState((prevState) => {
+            if(prevState.qty-1>=0){
+                return {
+                    qty: prevState.qty - 1
+                }
+            }else{
+                return {
+                    qty: 0
+                }
+            }
+            
+        })
+    }
+    
     render(){
         const {price, title, qty} = this.state;
         return (
@@ -24,9 +58,23 @@ class CartItem extends React.Component{
                     <div style={{fontSize: 25}} >Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
-                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png"></img>
-                        <img alt="decrease" className="action-icons" src="https://as2.ftcdn.net/jpg/02/44/39/19/500_F_244391909_DfS5TL9Oyzhf4VW0v9gPq4FPDg3BtD9w.jpg"></img>
-                        <img alt="delete" className="action-icons" src="https://as2.ftcdn.net/jpg/00/98/26/11/500_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg"></img>
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/992/992651.png"
+                            onClick= {this.increaseQuantity} 
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://as2.ftcdn.net/jpg/02/44/39/19/500_F_244391909_DfS5TL9Oyzhf4VW0v9gPq4FPDg3BtD9w.jpg"
+                            onClick = {this.decreaseQuantity}
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://as2.ftcdn.net/jpg/00/98/26/11/500_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg"
+                        />
                     </div>
                 </div>
             </div>
